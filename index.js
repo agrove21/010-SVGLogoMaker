c8f561ef90840419497f2ddc91bb491cfb70f957
Your Name <you@example.com>
2024-09-02 08:49:25 -0500

    did the run through

--- index.js
+++ index.js
@@ -1,38 +1,57 @@
-const inquirer = require('inquirer');
-const { Triangle, Circle, Square } = require('./lib/shape');
-const generateSvg = require('./lib/generateSvg');
+const inquirer = require("inquirer");
+const fs = require("fs");
+const { Square, Circle, Triangle } = require("./lib/shapes");
+const generateSvg = require("./lib/generateSvg");
+
+const prompt = inquirer.createPromptModule();
 
 async function init() {
-    const answers = await inquirer.prompt([
-        {
-            type: 'list',
-            name: 'shape',
-            message: 'Which shape would you like for your logo?',
-            choices: ['Triangle', 'Circle', 'Square'],
-        },
-        {
-            type: 'input',
-            name: 'color',
-            message: 'What color would you like the shape to be?',
+  const answers = await prompt([
+    {
+      type: "list",
+      name: "shape",
+      message: "Select a shape for your logo:",
+      choices: ["Square", "Circle", "Triangle"],
+    },
+    {
+      type: "input",
+      name: "color",
+      message: "What color would you like for your shape?",
+    },
+    {
+      type: "input",
+      name: "text",
+      message: "Enter up to three characters for your logo text:",
+      validate: function (input) {
+        if (input.length > 3) {
+          return "Text must be three characters or less.";
         }
-    ]);
+        return true;
+      },
+    },
+    {
+      type: "input",
+      name: "textColor",
+      message: "What color would you like for your text?",
+    },
+  ]);
 
-    let shape;
-    switch (answers.shape) {
-        case 'Triangle':
-            shape = new Triangle();
-            break;
-        case 'Circle':
-            shape = new Circle();
-            break;
-        case 'Square':
-            shape = new Square();
-            break;
-    }
+  let shape;
+  switch (answers.shape) {
+    case "Square":
+      shape = new Square();
+      break;
+    case "Circle":
+      shape = new Circle();
+      break;
+    case "Triangle":
+      shape = new Triangle();
+      break;
+  }
 
-    shape.setColor(answers.color);
-    generateSvg(shape);
-    console.log('Generated logo.svg');
+  shape.setColor(answers.color);
+  generateSvg(shape, answers.text, answers.textColor);
+  console.log("Generated logo.svg");
 }
 
 init();
